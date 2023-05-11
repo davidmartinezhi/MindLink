cff4c641ed577b6a3c48bb46ccdfc8b9b13bc5a7
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2023-05-11 16:10:35 -0600

    Initial Commit

---
+++ nuevoamanecer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nuevoamanecer
+//
+//  Created by Gerardo Martínez on 11/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nuevoamanecer/nuevoamanecerApp.swift
@@ -0,0 +1,17 @@
+//
+//  nuevoamanecerApp.swift
+//  nuevoamanecer
+//
+//  Created by Gerardo Martínez on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct nuevoamanecerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
